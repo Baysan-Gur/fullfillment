b2f41317e53cacc1b3822adc85ca81a9e8ed1c7e
Baysan-Gur <ba51k@bk.ru>
2025-05-31 20:19:38 +0300

    Создан проект

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./Main.module.css";
+import Calculator from "../Calculator/Calculator";
+
+const Main = ({ calculatorRef }) => {
+  const handleScroll = () => {
+    calculatorRef?.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  return (
+    <div className={`${styles.contentWrapper} ${styles.fadeIn}`}>
+      <div className={styles.mainContent}>
+        <div className={styles.descriptionBlock}>
+          <p className={styles.descriptionText}>
+            Компания <span className={styles.neonPink}>Формат24.рф</span> — это
+            команда профессионалов с уникальным опытом работы в администрации
+            Wildberries и практикой ведения собственных продаж на маркетплейсах.
+          </p>
+          <p className={styles.descriptionText2}>
+            Мы объединяем инсайдерские знания платформы с глубоким пониманием
+            потребностей продавцов, предлагая клиентам комплексные и эффективные
+            логистические решения.
+          </p>
+          <button onClick={handleScroll}>Рассчитать стоимость</button>
+        </div>
+
+        <div ref={calculatorRef} className={styles.scrollWrap}>
+          <Calculator />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
